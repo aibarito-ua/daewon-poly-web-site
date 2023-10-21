384a536bb40db999b1252477b9ed390fdafde1bf
임대원 <daewon@imdaewon-ui-MacBookAir.local>
2023-10-21 18:11:30 +0900

    update 231021 1800 issues

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App.v2023102111';
+import App from './App.v202310211800';
 
 test('renders learn react link', () => {
   render(<App />);
