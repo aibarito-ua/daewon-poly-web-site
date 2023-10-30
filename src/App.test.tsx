b119085b110c2b05f8f9736fcb65268dcd39de4d
임대원 <daewon@imdaewon-ui-MacBookAir.local>
2023-10-30 18:49:29 +0900

    update 231030 1900 version

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App.v202310292020';
+import App from './App.v202310301900';
 
 test('renders learn react link', () => {
   render(<App />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.v202310292020';
+import App from './App.v202310301900';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
